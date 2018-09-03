17deadd14d785fea06ebe88f2dc5388d985771fa
Thiago Galvani <thiagopaiva99@gmail.com>
2018-09-03 18:21:38 -0300

    Changin the Promise implementation from mongoose to default ES6 promise

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
-const options = { useNewUrlParser: true };
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/users_test', options);
-mongoose.connection
-    .once('open', () => console.log('Good to Go!'))
-    .on('error', error => console.warn('Error', error));
+before(done => {
+    const options = { useNewUrlParser: true };
+
+    mongoose.connect('mongodb://localhost:27017/users_test', options);
+    mongoose.connection
+        .once('open', () => done())
+        .on('error', error => console.warn('Error', error));
+});
 
 beforeEach(done => {
     mongoose.connection.collections.users.drop(() => {
