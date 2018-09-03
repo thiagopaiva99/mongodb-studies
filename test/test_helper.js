ba487c9f80317683761d8fc6e39c9e0e7677f016
Thiago Galvani <thiagopaiva99@gmail.com>
2018-09-03 15:59:18 -0300

    Fixing require, adding mongo port, adding options for connect method

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,8 @@
-const mongoose = requrie('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+const options = { useNewUrlParser: true };
+
+mongoose.connect('mongodb://localhost:27017/users_test', options);
 mongoose.connection
     .once('open', () => console.log('Good to Go!'))
     .on('error', error => console.warn('Error', error));
