47329002999bd7e4b9f91dced6a39d6b2cdb22ab
Thiago Galvani <thiagopaiva99@gmail.com>
2018-09-04 11:08:19 -0300

    Update model instances

--- test/update_test.js
+++ test/update_test.js
@@ -11,14 +11,26 @@ describe('Updating records', () => {
             .then(() => done());
     });
 
-    it('should update using instance set and save', done => {
-        thiago.set('name', 'Thiagooo');
-        thiago.save()
+    const assertName = (operation, done) => {
+        operation
             .then(() => User.find({ name: 'Thiagooo' }))
                 .then(users => {
                     assert(users.length === 1);
                     assert(users[0].name === 'Thiagooo');
                     done();
                 });
+    }
+
+    it('should update using instance set and save', done => {
+        thiago.set('name', 'Thiagooo');
+        assertName(thiago.save(), done);
+    });
+
+    it('should update a model instance with update', done => {
+        assertName(thiago.update({ name: 'Thiagooo' }), done);
+    });
+
+    it('should update a model class with update', done => {
+        assertName(User.update({ name: 'Thiago' }, { name: 'Thiagooo' }), done);
     });
 });
